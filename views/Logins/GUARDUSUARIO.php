3175274978748d83594171cbb9732031b7849f76
JackDLC326 <109977066+JackDLC326@users.noreply.github.com>
2022-08-14 16:50:24 -0500

    Update GUARDUSUARIO.php

--- views/Logins/GUARDUSUARIO.php
+++ views/Logins/GUARDUSUARIO.php
@@ -4,28 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>Usaurio registrado</title>
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <title>usuario</title>
 </head>
 <body>
-    <div class="container">
-        <?php
-        use MyApp\Query\Ejecuta; 
-        require("../../vendor/autoload.php");
+<?php
+    session_start();
+    if(isset($_SESSION["usuario"]))
+    {
+        echo "<h5 align'right'>usuario:".$_SESSION["usuarios"]."</h5>";
+        echo "<h6 align-'right'>
+        <a href='scripts/cerrarSesion.php'>[CerrarSesion]</a>
+        </h6>";
+    }
+    else
+    {
+        header("Location:login_app.php");
+    }
+    ?>
 
-        $usuario = new Ejecuta();
-        extract($_POST);
-        $HASH = password_hash($password, PASSWORD_DEFAULT);
-        $nivel1=(1);
-        $cadena="INSERT INTO usuario(nombres, apellidos, direccion, telefono, correo, f_nac, cp, contraseña, nivel_usuario) VALUES('$nombre','$apellido','$direccion','$telefono','$user','$fnac','$cp','$HASH','$nivel1')";
+    <?php
+    use MyApp\Query\Ejecuta;
+    require("../../vendor/autoload.php");
 
-        $usuario->ejecutar($cadena);
+    $usuario =new Ejecuta();
+    extract($_POST);
+    $hash = password_hash($pass, PASSWORD_DEFAULT);
+    $nivel=(1);
+    $cadena="INSERT INTO usuario(NOMBRES,APELLIDOS,DIRECCION,TELEFONO,CORREO,F_NAC,CP,CONTRASEÑA,NIVEL_USUARIO)
+     VALUES('$nombres','$apellidos','$dire','$tele','$correo','$FC','$CP','$hash','$nivel')";
 
-        echo "<div class='alert alert=success'>USUARIO REGISTRADO</div>";
-        header("refresh:7 login_app.php");
-        ?>
+    $usuario->ejecuta($cadena);
+
+    echo "<div class='alert alert-success'>USUARIO REGISTRADO</div>";
+    header("refresh:10 login_app.php");
+    ?>
     </div>
 </body>
 </html>
-
-
