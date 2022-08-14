3c110d3daddfe1b7176f90a2623c08ea52dffd23
JackDLC326 <109977066+JackDLC326@users.noreply.github.com>
2022-08-14 16:48:25 -0500

    Update validar.php

--- views/Logins/validar.php
+++ views/Logins/validar.php
@@ -4,21 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <title>LOGIN</title>
+    <link rel="stylesheet" href="../../css/bootstrap.min.css">
+    <title>Document</title>
 </head>
 <body>
     <div class="container">
         <?php
-        use MyApp\Query\Login;
-        require("../../vendor/autoload.php");
-
-        $usuario = new Login();
+        use MyApp\Query\login;
+        require ("../../vendor/autoload.php");
+        $user =new login();
         extract($_POST);
 
-        $usuario->verificaLogin("$user","$password");
+        $user->verificalogin("$usuario","$contraseña");
 
+        $user->verificaemple("$usuario","$contraseña");
+
+        $user->verificausuario("$usuario","$contraseña");
+
+        $user->error("$usuario","$contraseña");
         ?>
-    </div>
+    </div>    
+    
 </body>
 </html>
