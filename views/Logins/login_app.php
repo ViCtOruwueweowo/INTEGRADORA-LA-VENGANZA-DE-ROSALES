381df4d0fceea1e7b03c9cba60c6d553e5f62230
JackDLC326 <109977066+JackDLC326@users.noreply.github.com>
2022-08-14 17:20:28 -0500

    Update login_app.php

--- views/Logins/login_app.php
+++ views/Logins/login_app.php
@@ -10,18 +10,35 @@
     <title>Login App</title>
 </head>
 <body style="background-image:url(https://www.xtrafondos.com/descargar.php?id=4052&resolucion=3840x2160)">
+<?php
+    session_start();
+    if (isset($_SESSION["usuario"]))
+    {
+        echo "<div class='alert 'alert-warning'>
+        <h2 align='center'> Ya existe una sesion activa, usuario: ".$_SESSION["usuario"]."</h2>";
+        echo "<h3 align-'center'>
+        <a href='cerrarSesion.php'>[Cerrar Sesion]</a>
+        </h3>
+        </div>";
+    }
+    else
+    {
+        ?>
 <form action="validar.php" method="post">
-
-<h1 class="animate__animated animate__backInLeft">Iniciar Sesion</h1>
-
-<p>Usuario <input type="text" placeholder="Ingrese su nombre" name="usuario"></p>
-
-<p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="contraseña"></p>
-
-<input type="submit" value="Ingresar">
-<br>
-<br>
-<a href="" style="color:white"><b>No tengo cuenta</b></a>
-</form>  
+    
+    <h1 class="animate__animated animate__backInLeft">Iniciar Sesion</h1>
+    
+    <p>Usuario <input type="text" placeholder="Ingrese su nombre" name="usuario"></p>
+    
+    <p>Contraseña <input type="password" placeholder="Ingrese su contraseña" name="contraseña"></p>
+    
+    <input type="submit" value="Ingresar">
+    <br>
+    <br>
+    <a href="formALTAUSUARIO.php" style="color:white"><b>No tengo cuenta</b></a>
+</form>
+<?php
+    }
+        ?>
 </body>
 </html>
